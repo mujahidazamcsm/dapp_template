f484cb0bd5cf79cd1132e974f73de0f0b134bbba
Pham Trung Truc <hi@trucph.am>
2021-03-02 11:40:58 +0700

    Update solc to v0.8.0; add ganache network; migrate contracts to v0.8.0

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    develop: {
+      port: 8545,
+    },
+    ganache: {
+      port: 7545,
+      host: "127.0.0.1",
+      network_id: "*",
+    },
+  },
+  mocha: {
+    useColors: true,
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0",
+    },
+  },
+};
